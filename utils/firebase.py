fbff089d6467fa6514ea7a82ad7c390af76b3b63
Maheswara Reddy <prabhuletimahi1942@gmail.com>
2026-01-03 19:13:44 +0530

    Fix Firebase init using Streamlit secrets (no file path)

--- utils/firebase.py
+++ utils/firebase.py
@@ -1,11 +1,11 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os
+
 
 def init_firebase():
     if not firebase_admin._apps:
-        key_path = os.path.join(os.getcwd(), "firebase_key.json")
-        cred = credentials.Certificate(key_path)
+        cred = credentials.Certificate(dict(st.secrets["firebase"]))
         firebase_admin.initialize_app(cred)
 
     return firestore.client()
